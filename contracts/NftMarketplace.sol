afe1aa5c0c099178f778fa0299c4e2135cf7c94e
simsim <simeon.campbell@live.com>
2022-12-30 10:13:40 +0700

    listItem done

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -6,11 +6,73 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
 
 contract NftMarketplace {
-    // Main Functions
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
 
-    function listItem(address nftAddress, uint256 tokenId, uint256 price) external {
+    event itemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // NFT Contract Address -> NFT TokenID -> Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    ///////////////
+    // Modifiers //
+    ///////////////
+
+    // Checks if the Nft is listed already or not. To be added to the listItem() function
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    // Checks is the spender is the owner of the Nft to be listed
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    ////////////////////
+    // Main Functions //
+    ////////////////////
+
+    /*
+     * @notice Method for listing your NFT on the marketplace
+     * @param nftAddress: Address of the Nft
+     * @param tokenId: the TokenId of the Nft
+     * @param price: Sale price of listed Nft
+     * @dev We could hold the NFTs in escrow on the contract,
+     * but this way people still hold thier Nfts when listed.
+     */
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external notListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
         if (price <= 0) {
             revert NftMarketplace__PriceMustBeAboveZero();
         }
@@ -18,10 +80,12 @@ contract NftMarketplace {
         if (nft.getApproved(tokenId) != address(this)) {
             revert NftMarketplace__NotApprovedForMarketplace();
         }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit itemListed(msg.sender, nftAddress, tokenId, price);
     }
 }
 
-// 1. `listItem`: List NFT's on the marketplace
+// 1. `listItem`: List NFT's on the marketplace -- DONE
 // 2. `buyItem`: Buy NFT's
 // 3. `cancelItem`: Cancel a listing
 // 4. `updateListing`: Update price
