191d8e68589ab85a5694c3534a08d5701d7806ed
simsim <simeon.campbell@live.com>
2023-01-06 14:31:11 +0700

    marketplace tests 2

--- test/unit/nftMarketplace.test.js
+++ test/unit/nftMarketplace.test.js
@@ -5,7 +5,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("NftMarketplace unit tests", async function () {
-          let nftMarketplaceContract, nftMarketplace, basicNft, basicNftContract
+          let nftMarketplace, basicNft
           const PRICE = ethers.utils.parseEther("0.1")
           const TOKEN_ID = 1
 
@@ -20,15 +20,36 @@ const { developmentChains } = require("../../helper-hardhat-config")
               await basicNft.approve(nftMarketplace.address, TOKEN_ID)
           })
 
-          it("It lists and can be bought", async function () {
-              await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
-              const userConnectedNftMarketplace = nftMarketplace.connect(user)
-              await userConnectedNftMarketplace.buyItem(basicNft.address, TOKEN_ID, {
-                  value: PRICE,
+          describe("listItem", function () {
+              it("It lists and can be bought", async function () {
+                  await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  const userConnectedNftMarketplace = nftMarketplace.connect(user)
+                  await userConnectedNftMarketplace.buyItem(basicNft.address, TOKEN_ID, {
+                      value: PRICE,
+                  })
+                  const newOwner = await basicNft.ownerOf(TOKEN_ID)
+                  const deployerProceeds = await nftMarketplace.getProceeds(deployer)
+                  assert(newOwner.toString() == user.address)
+                  assert(deployerProceeds.toString() == PRICE.toString())
               })
-              const newOwner = await basicNft.ownerOf(TOKEN_ID)
-              const deployerProceeds = await nftMarketplace.getProceeds(deployer)
-              assert(newOwner.toString() == user.address)
-              assert(deployerProceeds.toString() == PRICE.toString())
+
+              it("Emits an event when NFT is listed", async function () {
+                  expect(await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)).to.emit(
+                      "ItemListed"
+                  )
+              })
+
+              it("Reverts if price is not above 0", async function () {
+                  expect(
+                      await nftMarketplace.listItem(basicNft.address, TOKEN_ID, 0)
+                  ).to.be.revertedWith("NftMarketplace__PriceMustBeAboveZero")
+              })
+
+              it("onlyOwner can list an NFT", async function () {
+                  NotOwner = nftMarketplace.connect(user)
+                  expect(
+                      await NotOwner.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  ).to.be.revertedWith("NftMarketplace__NotOwner")
+              })
           })
       })
